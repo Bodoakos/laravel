991fe87bbc2f423fed21a78c292ed86998a5f840
Bodoakos <118255525+Bodoakos@users.noreply.github.com>
2024-01-17 08:36:21 +0100

    upd

--- resources/views/adatok/edit.blade.php
+++ resources/views/adatok/edit.blade.php
@@ -3,12 +3,20 @@
 @error('name')
 <div class="alert alert-warning">{{$message}}</div>
 @enderror
-<form action="{{route('adatka.updtade',$adatok->id)}}" methode="post">
+<form action="{{route('edit',$adatok->id)}}" methode="post">
     @csrf
     methode('PUT')
     <fieldset>
         <label for="name">  Név:</label>
-        <input type="text" name="name" id="name" value="{{old('name',$adatok->name)}}"> 
+        <input type="text" name="name" id="name" value="{{old('name',$adatok->Név)}}">
+        <label for="name"> Márka: </label>
+        <input type="text" name="Márka" id="Márka" value="{{old('Márka',$adatok->Márka)}}">  
+        <label for="name"> Évjárat: </label>
+        <input type="date" name="Évjárat" id="Évjárat" value="{{old('Évjárat',$adatok->Évjárat)}}">  
+        <label for="name"> Km: </label>
+        <input type="number" name="Km" id="Km" value="{{old('Km',$adatok->km)}}">  
+        <label for="name"> Max Km/h: </label>
+        <input type="number" name="Km/h" id="Km/h" value="{{old('Km/h',$adatok->Maxkmh)}}">  
     </filedset>
     <button type="submit">ment</button>
 </form>
--- resources/views/adatok/index.blade.php
+++ resources/views/adatok/index.blade.php
@@ -7,14 +7,18 @@
 @endif
 <ul>
     @foreach($adatok as $a)
-    
+    <form action="{{route('destroy',$a->id)}}" method="Post">
+            @csrf
+           
     <li> Márka:{{$a->Márka}}
-       <ul> id:{{$a->id}}</ul><button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
-       <ul>Neve:{{$a->Név}}</ul><button>szerkeszt</button><button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
-       <ul>Évjárata:{{$a->Évjárat}}</ul><button>szerkeszt</button><button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
-       <ul>km:{{$a->km}}</ul><button>szerkeszt</button><button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
-       <ul>Max sebesség:{{$a->Maxkmh}}</ul><button>szerkeszt</button><button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
-       <form action="{{route('destroy($a->id)')}}" method="post">
+       <ul> id:{{$a->id}}</ul>
+       <ul>Neve:{{$a->Név}}</ul>
+       <ul>Évjárata:{{$a->Évjárat}}</ul>
+       <ul>km:{{$a->km}}</ul>
+       <ul>Max sebesség:{{$a->Maxkmh}}</ul>
+       @method('DELETE')
+            <button type7="submit" class="danger" onclick="return confirm('tutira törli?')">Törlés</button>
+    </form>
         
     
     </li>
